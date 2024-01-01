66c762dde2ab53e856fe904dc97bfac69b220db1
Osama Ahmed <osama.ahmed@check24.de>
2024-01-01 16:29:12 +0100

    fix

--- src/de/check24/energy/slack/BlockTemplate.groovy
+++ src/de/check24/energy/slack/BlockTemplate.groovy
@@ -36,30 +36,112 @@ class BlockTemplate {
     static def prepare(buildNumber, buildTag, status, triggeredBy) {
         return new Block([
                 [
-                        'type': 'header',
-                        'text': [
-                                'type': 'plain_text',
-                                'text': "Release ${buildNumber}  ${buildTag}"
+                        "type": "header",
+                        "text": [
+                                "type": "plain_text",
+                                "text": ":c24heartbeat: New release in progress",
+                                "emoji": true
                         ]
                 ],
                 [
-                        'type': 'section',
-                        'fields': [
+                        "type": "context",
+                        "elements": [
                                 [
-                                        'type': 'mrkdwn',
-                                        'text': "*Status:*\n ${status}"
+                                        "type": "mrkdwn",
+                                        "text": "*▷ Triggerd by @Osama Ahmed*"
+                                ]
+                        ]
+                ],
+                [
+                        "type": "section",
+                        "fields": [
+                                [
+                                        "type": "mrkdwn",
+                                        "text": "*⌁Tag:*\n> r20230915-105717"
                                 ],
                                 [
-                                        'type': 'mrkdwn',
-                                        'text': ' \n '
+                                        "type": "mrkdwn",
+                                        "text": "*⌁Job:*\n> *messagesystem*"
                                 ],
                                 [
-                                        'type': 'mrkdwn',
-                                        'text': "*Triggered By:*\n ${triggeredBy}"
+                                        "type": "mrkdwn",
+                                        "text": "*✎ Environment:*\n>Production"
+                                ],
+                                [
+                                        "type": "mrkdwn",
+                                        "text": "*⌁Status:*\n> Active (blue/green) :approve:"
+                                ]
+                        ]
+                ],
+                [
+                        "type": "divider"
+                ],
+                [
+                        "type": "context",
+                        "elements": [
+                                [
+                                        "type": "mrkdwn",
+                                        "text": ":jira:   *Tickets queued for release.*"
+                                ]
+                        ]
+                ],
+                [
+                        "type": "actions",
+                        "elements": [
+                                [
+                                        "type": "button",
+                                        "text": [
+                                                "type": "plain_text",
+                                                "emoji": true,
+                                                "text": ":jira2: FEATII-879"
+                                        ],
+                                        "value": "click_me_123"
+                                ],
+                                [
+                                        "type": "button",
+                                        "text": [
+                                                "type": "plain_text",
+                                                "emoji": true,
+                                                "text": ":jira2: FEATII-880"
+                                        ],
+                                        "value": "click_me_123"
+                                ],
+                                // ... (other button elements)
+                                [
+                                        "type": "button",
+                                        "text": [
+                                                "type": "plain_text",
+                                                "emoji": true,
+                                                "text": ":approve:   Activate"
+                                        ],
+                                        "style": "primary",
+                                        "value": "click_me_123"
+                                ],
+                                [
+                                        "type": "button",
+                                        "text": [
+                                                "type": "plain_text",
+                                                "emoji": true,
+                                                "text": ":needswork:  Rollback"
+                                        ],
+                                        "style": "primary",
+                                        "value": "click_me_123"
+                                ]
+                        ]
+                ],
+                [
+                        "type": "divider"
+                ],
+                [
+                        "type": "context",
+                        "elements": [
+                                [
+                                        "type": "mrkdwn",
+                                        "text": "*▷ Powered by F2. :c24tick:*"
                                 ]
                         ]
                 ]
-        ])
+                ])
     }
 
     Map issueMessage = [
