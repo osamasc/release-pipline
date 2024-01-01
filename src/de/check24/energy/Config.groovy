542bbe5525c7bd845188f3582098627824c86d93
Osama Ahmed <osama.ahmed@check24.de>
2024-01-01 04:47:05 +0100

    fix

--- src/de/check24/energy/Config.groovy
+++ src/de/check24/energy/Config.groovy
@@ -4,19 +4,10 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class Config {
 
-    public final static Map NODES = [
-            'Web': [
-                    'w01.telko.intern',
-            ],
-            'Admin': [
-                    'a01.telko.intern',
-            ]
-    ]
-
     private final static List DEFAULTSSHKEYS = [ 'bitbucket', 'deploy' ]
 
     private final static Map CHANNEL = [
-            'internet-it-pm': 'C031UXXXXJ1',
+            'energie-f2-monitoring': 'C031UXXXXJ1',
     ]
     public final static Map PROJECT = [
             'deploy.dev': [
@@ -25,14 +16,6 @@ class Config {
                             'Channel': CHANNEL['energie-f2-monitoring'],
                             'User': 'Development Deploy Release'
                     ],
-                    'Deploy': [
-                            'Type': 'RENAME',
-                            'User': 'pu_test_deploy',
-                            'DirBase': 'deploy.dev',
-                            'Nodes': [
-                                    NODES['web']
-                            ]
-                    ],
                     'SSH-Credentials': DEFAULTSSHKEYS
             ],
             'deploy.staging': [
@@ -40,14 +23,6 @@ class Config {
                             'Icon': ':toolbox:',
                             'Channel': CHANNEL['energie-f2-monitoring'],
                             'User': 'Staging Deploy Release'
-                    ],
-                    ' ': [
-                            'Type': 'RENAME',
-                            'User': 'pu_test_deploy',
-                            'DirBase': 'deploy.staging',
-                            'Nodes': [
-                                    NODES['web']
-                            ]
                     ],
                     'SSH-Credentials': DEFAULTSSHKEYS
             ]
