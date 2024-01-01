2a3cb6f21c61cbf13354e38edfbdf9e9eae0e827
Osama Ahmed <osama.ahmed@check24.de>
2024-01-01 05:23:14 +0100

    fix

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -4,26 +4,6 @@ import de.check24.energy.Config
 import de.check24.energy.Slack
 import de.check24.energy.slack.SlackResponse
 
-//void sendSuccess(String message) {
-//    sendNotification(message, 'success')
-//}
-//
-//void sendWarning(String message) {
-//    sendNotification(message, 'warning')
-//}
-//
-//void sendError(String message) {
-//    sendNotification(message, 'error')
-//}
-
-//void sendNotification(String message, String state = null) {
-//    String teamName = (JOB_NAME =~ /(.*)\//)[0][1]
-//    String channel = Config.PROJECT[JOB_BASE_NAME]['Slack']['Channel']
-//    String username = Config.PROJECT[JOB_BASE_NAME]['Slack']['User']
-//    String icon = Config.PROJECT[JOB_BASE_NAME]['Slack']['Icon']
-//    new Slack(this, teamName, JOB_BASE_NAME, channel, username, icon, [], 'slack').sendMessage(message, state)
-//}
-
 def sendReleaseMessage(String version = null, def state = Slack.BuildStatus.START) {
 
     if (!env.SLACK_TIMESTAMP && state != Slack.BuildStatus.START) {
@@ -33,7 +13,7 @@ def sendReleaseMessage(String version = null, def state = Slack.BuildStatus.STAR
     def gitContext = gitChangelog(
             returnType: 'CONTEXT',
             from: [type: 'REF', value: 'production'],
-            to: [type: 'COMMIT', value: GIT_COMMIT],
+            to: [type: 'COMMIT', value: env.GIT_COMMIT],
             customIssues: [
                     [
                             issuePattern: '([A-Z]+-[0-9]+)',
@@ -99,4 +79,24 @@ def sendReleaseMessage(String version = null, def state = Slack.BuildStatus.STAR
 //
 //String getUserName(String email) {
 //    return '<@' + new Slack(this, '', JOB_BASE_NAME, '', '', '', [], 'slack').getUserName(email) + '>'
+//}
+
+//void sendSuccess(String message) {
+//    sendNotification(message, 'success')
+//}
+//
+//void sendWarning(String message) {
+//    sendNotification(message, 'warning')
+//}
+//
+//void sendError(String message) {
+//    sendNotification(message, 'error')
+//}
+
+//void sendNotification(String message, String state = null) {
+//    String teamName = (JOB_NAME =~ /(.*)\//)[0][1]
+//    String channel = Config.PROJECT[JOB_BASE_NAME]['Slack']['Channel']
+//    String username = Config.PROJECT[JOB_BASE_NAME]['Slack']['User']
+//    String icon = Config.PROJECT[JOB_BASE_NAME]['Slack']['Icon']
+//    new Slack(this, teamName, JOB_BASE_NAME, channel, username, icon, [], 'slack').sendMessage(message, state)
 //}
