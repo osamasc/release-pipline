c397320da0d817924f5c72519da26862024e2a6c
Osama Ahmed <osama.ahmed@check24.de>
2024-01-01 04:51:12 +0100

    fix

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -23,9 +23,12 @@ import de.check24.energy.slack.SlackResponse
 //}
 
 void sendReleaseMessage(String version = null, def state = Slack.BuildStatus.START) {
+
     if (!env.SLACK_TIMESTAMP && state != Slack.BuildStatus.START) {
         return
     }
+
+    echo 'here'
 
     def gitContext = gitChangelog(
             returnType: 'CONTEXT',
