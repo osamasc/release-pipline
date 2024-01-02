903af64dbf0b4cd116703dd7cb224173c4cab91f
Osama Ahmed <osama.ahmed@check24.de>
2024-01-02 11:24:39 +0100

    ENFEAT-123: Add new stag

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -19,7 +19,7 @@ def sendReleaseMessage(String version = null, def state = Slack.BuildStatus.STAR
                 to: [type: 'COMMIT', value: env.GIT_COMMIT],
                 ignoreCommitsIfMessageMatches: '^Merge.*',
                 ignoreCommitsWithoutIssue: false,
-                jira: [server: 'https://c24-energie.atlassian.net/', username: 'osama.ahmed@check24.de', basicAuthString: JIRA_TOKEN],
+                jira: [server: 'https://c24-energie.atlassian.net/', username: 'osama.ahmed@check24.de', basicAuthString: 'xxxxx'],
                 customIssues: [
                         [
                                 issuePattern: '([A-Z]+-[0-9]+)',
